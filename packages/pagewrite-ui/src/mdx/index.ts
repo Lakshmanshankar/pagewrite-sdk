8b56b94bce7964fbc181c15ed07c916088c34a4a
Lakshmanshankar <lakshmanshankarc@gmail.com>
2026-09-12 12:17:17 +0530

    fix typo in mdx export file

--- packages/pagewrite-ui/src/mdx/index.ts
+++ packages/pagewrite-ui/src/mdx/index.ts
@@ -2,5 +2,5 @@ export { default as Callout } from "./Callout.astro";
 export { default as Image } from "./Image.astro";
 export { default as Spacer } from "./Spacer.astro";
 export { default as Columns } from "./Columns.astro";
-export { default as ColumnItem } from "./ColumnItem.astro";k
+export { default as ColumnItem } from "./ColumnItem.astro";
 export { default as ContactForm } from "./ContactForm.astro";
